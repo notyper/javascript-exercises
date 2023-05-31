e311279889dfb2f2a61f9efc3f3ea5e5d48a08f6
Kevin Jackman <notyper@gmail.com>
2023-05-31 16:29:19 -0400

    Complete first 4 assignments

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  const chars = string.split("");
+  chars.reverse();
+  return chars.join("")
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function (theArray, ...items) {
+    const temp = [];
+    let flag = false;
+    for (const arrayItem of theArray) {
+        flag = false;
+        for (const item of items) {
 
-};
+            if (arrayItem === item) {
+                flag = true;
+            }
+        }
+        if (flag === false) {
+            temp.push(arrayItem);
+        }
+//        console.log(`the array = ${temp}`);
+    }
+    return temp;
+
+}
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
