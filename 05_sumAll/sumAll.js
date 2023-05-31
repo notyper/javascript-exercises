b9fb88bf6741bcdfb2a4ee246754a21bfe6ad29d
Kevin Jackman <notyper@gmail.com>
2023-05-31 17:20:11 -0400

    Complete excerise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (int1, int2) {
+    if (!Number.isInteger(int1) 
+     || !Number.isInteger(int2) 
+     ||  (int1 < 0 || int2 < 0)) {
+        return "ERROR"
+    }
 
+    let sum = 0
+    let count = Math.min(int1, int2);
+    let highest = Math.max(int1, int2);
+
+    do {
+        sum += count++;
+    } while (count <= highest);
+    return sum;
 };
 
 // Do not edit below this line
